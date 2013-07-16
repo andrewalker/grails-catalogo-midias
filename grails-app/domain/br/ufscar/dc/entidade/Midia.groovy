d9c4e96c8b6dac597b922bc7a7da62fdc3060f55
André Walker <andre@andrewalker.net>
2013-07-16 09:19:42 -0300

    Add relationships

--- grails-app/domain/br/ufscar/dc/entidade/Midia.groovy
+++ grails-app/domain/br/ufscar/dc/entidade/Midia.groovy
@@ -2,6 +2,8 @@ package br.ufscar.dc.entidade
 
 class Midia {
 
+    static belongsTo = Usuario
+
     String titulo
     int ano
 
--- grails-app/domain/br/ufscar/dc/entidade/Usuario.groovy
+++ grails-app/domain/br/ufscar/dc/entidade/Usuario.groovy
@@ -2,6 +2,8 @@ package br.ufscar.dc.entidade
 
 class Usuario {
 
+    static hasMany = [ midias: Midia ]
+
     String nome
     String login
     String senha
